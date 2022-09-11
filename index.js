b6729113b43fc4423f96cb020d1a3fbf9f1fd7e1
TCdotmake <tommyjchen@hotmail.com>
2022-09-10 21:24:55 -0400

    input parsing and submit working for required

--- index.js
+++ index.js
@@ -15,4 +15,37 @@ let test = {
 };
 
 let newBook = new Book(test);
-console.table(newBook);
+console.log(newBook);
+let values = [];
+function handleSubmit(e) {
+  e.preventDefault();
+  const inputValues = document.querySelectorAll("input");
+
+  let valid = true;
+  for (let ele of inputValues) {
+    if (ele.type === "text" || ele.type === "number") {
+      if (ele.value === "") {
+        valid = false;
+      }
+    }
+  }
+  if (valid) {
+    for (let ele of inputValues) {
+      if (ele.type === "checkbox") {
+        values.push(ele.checked);
+        ele.checked = false;
+      } else {
+        values.push(ele.value);
+        ele.value = "";
+      }
+    }
+    [title, author, pages, read] = values;
+    myLibrary.push(new Book({ title, author, pages, read }));
+    console.log("myLibrary", myLibrary);
+  }
+}
+
+const submitBtn = document.getElementById("submit-new");
+submitBtn.addEventListener("click", (e) => {
+  handleSubmit(e);
+});
